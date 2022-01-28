b8e8d422560a1770c1e202b8109d2542bbc01d36
Joshua Davis <jdavis123@huskers.unl.edu>
2022-01-28 17:09:11 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -11,7 +11,9 @@
 
 int main(int argc, char **argv) {
 
-  printf("Joshua Davis Aidan Brammeier\n");
+  printf("Joshua Davis Aidan Brammeier!\n");
+  printf("Computer Engineering\n");
+
 
   return 0;
 }
