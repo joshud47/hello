c47f2ce9a16180445bd3721bd1d36220cb2e4797
Joshua Davis <jdavis123@huskers.unl.edu>
2022-01-28 16:51:41 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Joshua Davis Aidan Brammeier
+ *         jdavis123@huskers.unl.edu
+ * Date: 2022/01/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joshua Davis Aidan Brammeier\n");
+
+  return 0;
+}
