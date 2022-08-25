c69f80cec9787f181ba2d6a4cde9e83f678349c2
Damien Beatson <dbeatson2@huskers.unl.edu>
2022-08-25 21:45:07 +0000

    added my name and major to the output

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv) {
 
   printf("Joshua Davis!\n");
   printf("Computer Engineering\n");
+  printf("My name is Damien Beatson!\n");
+  printf("My major is computer engineering\n");
 
   return 0;
 }
